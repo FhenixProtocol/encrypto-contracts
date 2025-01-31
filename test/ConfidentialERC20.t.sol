7194e28706af7126fc22f16698302981ba256c1f
architect-dev <halftonedev@gmail.com>
2025-01-31 09:01:08 +0100

    Add tests for ConfidentialERC20, extract TestSetup package and create ERC20_Harness test contract

---
+++ test/ConfidentialERC20.t.sol
@@ -0,0 +1,133 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {console} from "forge-std/Test.sol";
+import {FHERC20} from "./FHERC20_Harness.sol";
+import {ERC20_Harness} from "./ERC20_Harness.sol";
+import {ConfidentialERC20} from "../src/ConfidentialERC20.sol";
+import {TestSetup} from "./TestSetup.sol";
+
+contract FHERC20Test is TestSetup {
+    function setUp() public override {
+        super.setUp();
+    }
+
+    // TESTS
+
+    function test_Constructor() public view {
+        assertEq(
+            eBTC.name(),
+            "Confidential Wrapped BTC",
+            "ConfidentialERC20 name correct"
+        );
+        assertEq(eBTC.symbol(), "eBTC", "ConfidentialERC20 symbol correct");
+        assertEq(eBTC.decimals(), 8, "ConfidentialERC20 decimals correct");
+        assertEq(
+            address(eBTC.erc20()),
+            address(wBTC),
+            "ConfidentialERC20 underlying ERC20 correct"
+        );
+    }
+
+    function test_Symbol() public {
+        ERC20_Harness TEST = new ERC20_Harness("Test Token", "TEST", 18);
+        ConfidentialERC20 eTEST = new ConfidentialERC20(TEST, "");
+
+        assertEq(eTEST.name(), "Confidential Test Token", "eTEST name correct");
+        assertEq(eTEST.symbol(), "eTEST", "eTEST symbol correct");
+        assertEq(eTEST.decimals(), TEST.decimals(), "eTEST decimals correct");
+        assertEq(
+            address(eTEST.erc20()),
+            address(TEST),
+            "eTEST underlying ERC20 correct"
+        );
+
+        eTEST.updateSymbol("encTEST");
+        assertEq(eTEST.symbol(), "encTEST", "eTEST symbol updated correct");
+    }
+
+    function test_encrypt() public {
+        assertEq(eBTC.totalSupply(), 0, "Total supply init 0");
+
+        // Mint wBTC
+        wBTC.mint(bob, 10e8);
+        vm.prank(bob);
+        wBTC.approve(address(eBTC), 10e8);
+
+        // 1st TX, indicated + 5001, true + 1e8
+
+        uint256 value = 1e8;
+
+        _prepExpectERC20BalancesChange(wBTC, bob);
+        _prepExpectFHERC20BalancesChange(eBTC, bob);
+
+        _expectERC20Transfer(wBTC, bob, address(eBTC), value);
+        _expectFHERC20Transfer(eBTC, address(0), bob);
+
+        vm.prank(bob);
+        eBTC.encrypt(bob, value);
+
+        _expectERC20BalancesChange(wBTC, bob, -1 * int256(value));
+        _expectFHERC20BalancesChange(
+            eBTC,
+            bob,
+            _ticksToIndicated(eBTC, 5001),
+            int256(value)
+        );
+
+        assertEq(eBTC.totalSupply(), value, "Total supply increases");
+
+        // 2nd TX, indicated + 1, true + 1e8
+
+        _prepExpectERC20BalancesChange(wBTC, bob);
+        _prepExpectFHERC20BalancesChange(eBTC, bob);
+
+        _expectERC20Transfer(wBTC, bob, address(eBTC), value);
+        _expectFHERC20Transfer(eBTC, address(0), bob);
+
+        vm.prank(bob);
+        eBTC.encrypt(bob, value);
+
+        _expectERC20BalancesChange(wBTC, bob, -1 * int256(value));
+        _expectFHERC20BalancesChange(
+            eBTC,
+            bob,
+            _ticksToIndicated(eBTC, 1),
+            int256(value)
+        );
+    }
+
+    function test_decrypt() public {
+        assertEq(eBTC.totalSupply(), 0, "Total supply init 0");
+
+        // Mint and encrypt wBTC
+        wBTC.mint(bob, 10e8);
+        vm.prank(bob);
+        wBTC.approve(address(eBTC), 10e8);
+        vm.prank(bob);
+        eBTC.encrypt(bob, 10e8);
+
+        // TX
+
+        uint256 value = 1e8;
+
+        _prepExpectERC20BalancesChange(wBTC, bob);
+        _prepExpectFHERC20BalancesChange(eBTC, bob);
+
+        _expectFHERC20Transfer(eBTC, bob, address(0));
+        _expectERC20Transfer(wBTC, address(eBTC), bob, value);
+
+        vm.prank(bob);
+        eBTC.decrypt(bob, value);
+
+        _expectERC20BalancesChange(wBTC, bob, int256(value));
+        _expectFHERC20BalancesChange(
+            eBTC,
+            bob,
+            -1 * _ticksToIndicated(eBTC, 1),
+            -1 * int256(value)
+        );
+
+        assertEq(eBTC.totalSupply(), 10e8 - value, "Total supply decreases");
+    }
+}
--- test/ERC20_Harness.sol
+++ test/ERC20_Harness.sol
@@ -3,14 +3,22 @@
 
 pragma solidity ^0.8.20;
 
-import {FHERC20} from "../src/FHERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20//ERC20.sol";
 
-contract FHERC20_Harness is FHERC20 {
+contract ERC20_Harness is ERC20 {
+    uint8 _decimals;
+
     constructor(
         string memory name_,
         string memory symbol_,
         uint8 decimals_
-    ) FHERC20(name_, symbol_, decimals_) {}
+    ) ERC20(name_, symbol_) {
+        _decimals = decimals_;
+    }
+
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
 
     function mint(address account, uint256 value) public {
         _mint(account, value);
