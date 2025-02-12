0bb9033c0a24d421c5a0bf5048d016042de430ca
architect-dev <halftonedev@gmail.com>
2025-02-12 10:17:01 +0100

    Work on TaskManagerMocks

---
+++ test/mocks/addresses/TaskManagerAddress.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+pragma solidity ^0.8.24;
+
+// todo (eshel) update with correct address
+address constant taskManagerAddress = 0x218A38A3f2A3EA3E8e092509C75e74BeB0adFB65;
